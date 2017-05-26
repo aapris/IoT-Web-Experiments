17b6dc0227154e8f4622b9f1a7c6c576c349643c
Aapo Rista <aapo.gitpub@rista.net>
2017-05-26 11:35:34 +0300

    Show Request model in admin

--- iotendpoints/endpoints/admin.py
+++ iotendpoints/endpoints/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from endpoints.models import Request
+
+
+class RequestAdmin(admin.ModelAdmin):
+    search_fields = ('method', 'status', )
+    list_display = ('method', 'status', 'created', 'filecount', )
+    ordering = ('created', )
+
+admin.site.register(Request, RequestAdmin)
