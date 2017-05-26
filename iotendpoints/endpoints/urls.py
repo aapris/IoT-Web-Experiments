94e13e5d00dc5e2782cf4a1346f098e3c2ad2fc0
Aapo Rista <aapo.gitpub@rista.net>
2017-05-26 10:53:14 +0300

    Add warning if OBSCURE_URL env var is not set

--- iotendpoints/endpoints/urls.py
+++ iotendpoints/endpoints/urls.py
@@ -2,9 +2,15 @@ import os
 from django.conf.urls import url
 from . import views
 
-OBSCURE_URL = r'^{}$'.format(os.environ.get('OBSCURE_URL', 'this_should_be_in_env_var'))
+
+DUMMY_OBSCURE_URL = 'this_should_be_in_env_var'
+OBSCURE_URL = os.environ.get('OBSCURE_URL', DUMMY_OBSCURE_URL)
+if DUMMY_OBSCURE_URL == OBSCURE_URL:
+    print("Warning: you should set OBSCURE_URL environment variable in this env\n\n")
+
+OBSCURE_URL_PATTERN = r'^{}$'.format(os.environ.get('OBSCURE_URL', OBSCURE_URL))
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(OBSCURE_URL, views.obscure_dump_request_endpoint, name='dump_request'),
+    url(OBSCURE_URL_PATTERN, views.obscure_dump_request_endpoint, name='dump_request'),
 ]
