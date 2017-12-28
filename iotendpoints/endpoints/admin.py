bde5a16590b006240cb9d0ef2750469d5fba30ba
Aapo Rista <aapo.rista@forumvirium.fi>
2017-12-28 12:44:12 +0200

    Add user column

--- iotendpoints/endpoints/admin.py
+++ iotendpoints/endpoints/admin.py
@@ -4,8 +4,8 @@ from endpoints.models import Request
 
 
 class RequestAdmin(admin.ModelAdmin):
-    search_fields = ('method', 'status', )
-    list_display = ('method', 'status', 'created', 'filecount', )
+    search_fields = ('method', 'status', 'user')
+    list_display = ('method', 'status', 'user', 'created', 'filecount', )
     ordering = ('created', )
 
 admin.site.register(Request, RequestAdmin)
