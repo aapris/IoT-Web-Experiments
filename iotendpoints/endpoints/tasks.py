7de9acdd116b259dca8181c2d2b3058da178be86
Aapo Rista <aapo.gitpub@rista.net>
2018-05-03 15:06:30 +0300

    Add save_to_influxdb task

--- iotendpoints/endpoints/tasks.py
+++ iotendpoints/endpoints/tasks.py
@@ -1,11 +1,29 @@
-# Create your tasks here
-from __future__ import absolute_import, unicode_literals
+import requests
+import json
 from celery import shared_task
+from celery.utils.log import get_task_logger
+from influxdb.exceptions import InfluxDBClientError
+
+from endpoints.utils import get_influxdb_client
+
+logger = get_task_logger(__name__)
 
 
-#def handle_datapost(headers, vars):
 @shared_task
-def handle_datapost(headers, vars):
-    print(headers, vars)
-    return headers, vars
+def save_to_influxdb(dbname, measurements):
+    """
+    Save valid `measurements` dictionary into InfluxDB database `dbname`.
+    Log errors.
+    :param dbname: Database name
+    :param measurements: a valid InfluxDB dictionary.
+    """
+    iclient = get_influxdb_client(database=dbname)
+    try:
+        iclient.create_database(dbname)
+        iclient.write_points(measurements)
+        logger.info('Successfully saved InfluxDB object to database {}'.format(dbname))  # Goes to celery log
+    except InfluxDBClientError as err:
+        err_msg = '[InfluxDB] {}'.format(err)
+        logger.error(err_msg)
+        logger.error(json.dumps(measurements))
 
