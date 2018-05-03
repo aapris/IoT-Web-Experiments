eecde225f67bccb9a166783b53c5d87e17a82f81
Aapo Rista <aapo.gitpub@rista.net>
2018-05-03 16:14:07 +0300

    Remove hardcoded sentilo (is in plugin now)

--- iotendpoints/endpoints/urls.py
+++ iotendpoints/endpoints/urls.py
@@ -14,7 +14,6 @@ if DUMMY_OBSCURE_URL == OBSCURE_URL:
 
 OBSCURE_URL_PATTERN = r'^{}(.*)$'.format(os.environ.get('OBSCURE_URL', OBSCURE_URL))
 DIGITA_URL_PATTERN = r'^{}$'.format(os.environ.get('DIGITA_URL', OBSCURE_URL))
-SENTILO_URL_PATTERN = r'^{}$'.format(os.environ.get('SENTILO_URL', OBSCURE_URL))
 
 # Load urlpatterns from plugins directory
 urlpatterns = plugin_urlpatterns(plugins)
@@ -23,7 +22,6 @@ urlpatterns += [
     url(r'^$', views.index, name='index'),
     url(OBSCURE_URL_PATTERN, views.obscure_dump_request_endpoint, name='dump_request'),
     url(DIGITA_URL_PATTERN, views.digita_dump_request_endpoint, name='digita_dump_request'),
-    url(SENTILO_URL_PATTERN, views.sentilohandler, name='sentilohandler'),
     url(r'^basicauth$', views.basicauth_dump_request_endpoint, name='basicauth_dump_request'),
     url(r'^aqtest$', views.basicauth_dump_request_endpoint, name='aqtest'),
     url(r'^fmiaq/v1$', views.fmiaqhandler, name='fmiaqhandler'),
