94a1b469e6be5f6a057b9e5231dd1b1b9144e03a
Aapo Rista <aapo.gitpub@rista.net>
2018-05-08 07:17:37 +0300

    Add Datalogger model admin

--- iotendpoints/endpoints/admin.py
+++ iotendpoints/endpoints/admin.py
@@ -1,11 +1,19 @@
-from django.contrib import admin
-
-from endpoints.models import Request
+from django.contrib.gis import admin
+from endpoints.models import Request, Datalogger
 
 
 class RequestAdmin(admin.ModelAdmin):
     search_fields = ('method', 'status', 'user')
-    list_display = ('method', 'status', 'user', 'created', 'filecount', )
-    ordering = ('created', )
+    list_display = ('method', 'status', 'user', 'created', 'filecount',)
+    ordering = ('created',)
+
 
 admin.site.register(Request, RequestAdmin)
+
+
+class DataloggerAdmin(admin.OSMGeoAdmin):
+    search_fields = ('name', 'description', 'devid')
+    list_display = ('devid', 'location', 'name', 'created_at', 'activity_at',)
+    ordering = ('created_at',)
+
+admin.site.register(Datalogger, DataloggerAdmin)
